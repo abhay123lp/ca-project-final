afade62c63af15ec3b04d7d5655bfb01a426f5ac
holdthehand <holdthehand@gmail.com>
2014-12-28 13:19:43 +0000

    Fix some properties and modified some of service classes

--- CAProject/trunk/CAProject/src/webservice/ImplUpload.java
+++ CAProject/trunk/CAProject/src/webservice/ImplUpload.java
@@ -1,7 +1,5 @@
 package webservice;
 
-
-
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -11,6 +9,8 @@ import java.io.IOException;
 
 import javax.xml.ws.WebServiceException;
 
+import util.PropertyLoader;
+
 /**
  * @author annvcit
  * @full_name Nguyen Van Chuc An
@@ -18,46 +18,43 @@ import javax.xml.ws.WebServiceException;
  * @date_created Dec 25, 2014
  *
  */
-public class ImplUpload implements IUpload{
+public class ImplUpload implements IUpload {
+	public static final String filePath = PropertyLoader.loadProperty("client_upload_folder");
+	@Override
+	public void upload(String fileName, byte[] fileBytes) {
+		 
+		try {
+			FileOutputStream fos = new FileOutputStream(filePath+fileName);
+			BufferedOutputStream outputStream = new BufferedOutputStream(fos);
+			outputStream.write(fileBytes);
+			outputStream.close();
 
+			System.out.println("Received file: " + (filePath +fileName));
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			System.err.println(ex);
+			throw new WebServiceException(ex);
+		}
+	}
 
-    @Override
-    public void upload(String fileName, byte[] fileBytes) {
-String filePath = "/home/annvcit/Desktop/test_webservice/server/upload/" + fileName;
-        
-        try {
-            FileOutputStream fos = new FileOutputStream(filePath);
-            BufferedOutputStream outputStream = new BufferedOutputStream(fos);
-            outputStream.write(fileBytes);
-            outputStream.close();
-            
-            System.out.println("Received file: " + filePath);
-            
-        } catch (IOException ex) {
-            System.err.println(ex);
-            throw new WebServiceException(ex);
-        }
-    }
+	@Override
+	public byte[] download(String fileName) {
+		String filePath = PropertyLoader.loadProperty("client_upload_folder") + fileName;
+		System.out.println("Sending file: " + filePath);
 
- 
-    @Override
-    public byte[] download(String fileName) {
-        String filePath = "/home/annvcit/Desktop/test_webservice/server/download/" + fileName;
-        System.out.println("Sending file: " + filePath);
-        
-        try {
-            File file = new File(filePath);
-            FileInputStream fis = new FileInputStream(file);
-            BufferedInputStream inputStream = new BufferedInputStream(fis);
-            byte[] fileBytes = new byte[(int) file.length()];
-            inputStream.read(fileBytes);
-            inputStream.close();
-            
-            return fileBytes;
-        } catch (IOException ex) {
-            System.err.println(ex);
-            throw new WebServiceException(ex);
-        }       
-    }
+		try {
+			File file = new File(filePath);
+			FileInputStream fis = new FileInputStream(file);
+			BufferedInputStream inputStream = new BufferedInputStream(fis);
+			byte[] fileBytes = new byte[(int) file.length()];
+			inputStream.read(fileBytes);
+			inputStream.close();
+
+			return fileBytes;
+		} catch (IOException ex) {
+			System.err.println(ex);
+			throw new WebServiceException(ex);
+		}
+	}
 
 }
